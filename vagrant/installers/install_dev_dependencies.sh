c2f0aa463dc0031192ca9e8713a4ebefd827688a
Marcelo Jacobus <marcelo.jacobus@xing.com>
2017-07-13 09:11:42 +0200

    Install yarn

--- vagrant/installers/install_dev_dependencies.sh
+++ vagrant/installers/install_dev_dependencies.sh
@@ -22,3 +22,11 @@ sudo apt-get install -y libzmq3-dev
 sudo apt-get install -y nodejs
 
 sudo apt-get install -y mysql-client
+
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+sudo apt-get update && sudo apt-get install -y yarn
+
+ln -s /usr/bin/nodejs /usr/bin/node
+
